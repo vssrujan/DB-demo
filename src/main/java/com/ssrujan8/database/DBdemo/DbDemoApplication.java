c292edbe20a375b2893221a36cb6a189b86f9061
vssrujan <saisr@Srujan>
2023-01-02 22:31:16 +0530

    First commit

---
+++ src/main/java/com/ssrujan8/database/DBdemo/DbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ssrujan8.database.DBdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssrujan8/database/DBdemo/DbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssrujan8.database.DBdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
